bdb53c300b437c4baef683374fdea52ec2baebbb
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-07-26 22:52:18 +0530

    initialised signup and signin page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from '@clerk/themes'
+import Header from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "StatusFlow",
@@ -23,12 +30,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark,
+    }}
+
+    >
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.className} animated-dotted-background`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-gray-400 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Copyright 2025 @StatusFlow.</p>
+            </div>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
