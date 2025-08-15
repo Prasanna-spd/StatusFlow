54f55e4b8ef2742342b8aa59fc079bdc4ea75b34
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-08-15 23:43:07 +0530

    ui bug fixing toatser

--- app/(main)/organization/[orgId]/_components/user-issues.jsx
+++ app/(main)/organization/[orgId]/_components/user-issues.jsx
@@ -46,3 +46,40 @@ function IssueGrid({ issues }) {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import java.util.Scanner;
+
+class InputExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Create Scanner object
+
+
+         System.out.print("Enter your age: ");
+        int age = sc.nextInt(); // Reads an integer
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine(); // Reads a whole line
+
+       
+
+        System.out.println("Hello, " + name + ". You are " + age + " years old.");
+
+        sc.close(); // Close scanner
+    }
+}
+
+
+*/
--- app/(main)/project/[projectId]/layout.tsx
+++ app/(main)/project/[projectId]/layout.tsx
@@ -3,7 +3,7 @@ import { BarLoader } from "react-spinners";
 
 export default async function ProjectLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto">
+    <div className="mx-auto"> 
       <Suspense fallback={<BarLoader width={"100%"} color="#36d7b7" />}>
         {children}
       </Suspense>
